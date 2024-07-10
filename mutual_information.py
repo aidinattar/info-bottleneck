073ab3f517bb2ef6e41dc70f02f99f2b1630c06d
aidinattar <attaraidin@gmail.com>
2024-07-10 22:38:34 +0200

    debug binning method

--- mutual_information.py
+++ mutual_information.py
@@ -9,8 +9,8 @@
 # Python version:   3.11.7                                                     #
 ################################################################################
 
-import torch
 import numpy as np
+import torch
 from sklearn.neighbors import NearestNeighbors
 from scipy.special import digamma, logsumexp
 
@@ -51,103 +51,171 @@ class MutualInformationCalculator:
             layer_data = layer_data.reshape(-1, 1)
 
         p_xs, unique_inverse_x = get_probabilities(input_data)
-        bins = np.linspace(-1, 1, num_bins, dtype='float32') 
-        digitized = bins[np.digitize(np.squeeze(layer_data.reshape(1, -1)), bins) - 1].reshape(len(layer_data), -1)
-        p_ts, _ = get_probabilities(digitized)
+        print(p_xs, unique_inverse_x)
+        
+        bins = np.linspace(-1, 1, num_bins, dtype='float32')
+        digitized_layer = bins[np.digitize(np.squeeze(layer_data.reshape(1, -1)), bins) - 1].reshape(len(layer_data), -1)
+        print(digitized_layer)
+        
+        p_ts, _ = get_probabilities(digitized_layer)
+        print(p_ts)
         
         h_layer = -np.sum(p_ts * np.log(p_ts + np.finfo(float).eps))  # Adding epsilon to avoid log(0)
+        print(h_layer)
         h_layer_given_input = 0.
         for xval in np.arange(len(p_xs)):
-            p_t_given_x, _ = get_probabilities(digitized[unique_inverse_x == xval, :])
+            p_t_given_x, _ = get_probabilities(digitized_layer[unique_inverse_x == xval, :])
             h_layer_given_input += - p_xs[xval] * np.sum(p_t_given_x * np.log(p_t_given_x + np.finfo(float).eps))
-        
+            print(h_layer_given_input)
+
         return h_layer - h_layer_given_input
 
-    def mutual_info_kde(self, X, Y):
-        def compute_pairwise_distances(matrix):
-            squared_sum = np.sum(np.square(matrix), axis=1, keepdims=True)
-            dists = squared_sum + squared_sum.T - 2 * np.dot(matrix, matrix.T)
-            return dists
+    def kde_mutual_info(self, X, Y, bandwidth):
+        """
+        Calculate mutual information using kernel density estimation.
 
-        def get_shape_info(tensor):
-            dims = float(tensor.shape[1])
-            N = float(tensor.shape[0])
-            return dims, N
+        Parameters
+        ----------
+        X : array-like
+            Input data.
+        Y : array-like
+            Output data.
+        bandwidth : float
+            Bandwidth for KDE.
+        
+        Returns
+        -------
+        mi : float
+            Mutual information between X and Y.
+        """
+        raise NotImplementedError("Not implemented yet")
+        # def compute_pairwise_distances(matrix):
+        #     squared_sum = np.sum(np.square(matrix), axis=1, keepdims=True)
+        #     dists = squared_sum + squared_sum.T - 2 * np.dot(matrix, matrix.T)
+        #     return dists
 
-        def estimate_entropy_kl(tensor, var):
-            dims, N = get_shape_info(tensor)
-            dists = compute_pairwise_distances(tensor)
-            dists2 = dists / (2 * var)
-            normconst = (dims / 2.0) * np.log(2 * np.pi * var)
-            lprobs = logsumexp(-dists2, axis=1) - np.log(N) - normconst
-            h = -np.mean(lprobs)
-            return dims / 2 + h
+        # def get_shape_info(tensor):
+        #     dims = float(tensor.shape[1])
+        #     N = float(tensor.shape[0])
+        #     return dims, N
 
-        def estimate_entropy_bd(tensor, var):
-            dims, N = get_shape_info(tensor)
-            val = estimate_entropy_kl(tensor, 4 * var)
-            return val + np.log(0.25) * dims / 2
+        # def estimate_entropy_kl(tensor, var):
+        #     dims, N = get_shape_info(tensor)
+        #     dists = compute_pairwise_distances(tensor)
+        #     dists2 = dists / (2 * var)
+        #     normconst = (dims / 2.0) * np.log(2 * np.pi * var)
+        #     lprobs = logsumexp(-dists2, axis=1) - np.log(N) - normconst
+        #     h = -np.mean(lprobs)
+        #     return dims / 2 + h
 
-        if isinstance(X, torch.Tensor):
-            X = X.cpu().detach().numpy()
-        if isinstance(Y, torch.Tensor):
-            Y = Y.cpu().detach().numpy()
+        # def estimate_entropy_bd(tensor, var):
+        #     dims, N = get_shape_info(tensor)
+        #     val = estimate_entropy_kl(tensor, 4 * var)
+        #     return val + np.log(0.25) * dims / 2
 
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-        if Y.ndim == 1:
-            Y = Y.reshape(-1, 1)
+        # if isinstance(X, torch.Tensor):
+        #     X = X.cpu().detach().numpy()
+        # if isinstance(Y, torch.Tensor):
+        #     Y = Y.cpu().detach().numpy()
 
-        var = self.bandwidth
-        hX = estimate_entropy_bd(X, var)
-        hY = estimate_entropy_bd(Y, var)
-        hXY = estimate_entropy_bd(np.hstack((X, Y)), var)
+        # var = bandwidth
+        # hX = estimate_entropy_bd(X, var)
+        # hY = estimate_entropy_bd(Y, var)
+        # hXY = estimate_entropy_bd(np.hstack((X, Y)), var)
 
-        return hX + hY - hXY
+        # return hX + hY - hXY
 
     def mutual_info_kraskov(self, X, Y):
-        if X.ndim == 1:
-            X = X.reshape(-1, 1)
-        if Y.ndim == 1:
-            Y = Y.reshape(-1, 1)
+        """
+        Calculate mutual information using Kraskov method.
 
+        Parameters
+        ----------
+        X : array-like
+            Input data.
+        Y : array-like
+            Output data.
+        
+        Returns
+        -------
+        mi : float
+            Mutual information between X and Y.
+        """
+        raise NotImplementedError("Not implemented yet")
+        X = X.reshape(-1, 1)
+        Y = Y.reshape(-1, 1)
         data = np.hstack((X, Y))
-        
+
         n = len(X)
-        d = data.shape[1]  # Dimensionality
-        
+        d = 2
+
         nbrs = NearestNeighbors(n_neighbors=self.k+1, metric='chebyshev').fit(data)
         distances, _ = nbrs.kneighbors(data)
-        
+
         epsilon = distances[:, self.k].reshape(n, 1)
-        
+
         nbrs_x = NearestNeighbors(n_neighbors=self.k, metric='chebyshev').fit(X)
         nbrs_y = NearestNeighbors(n_neighbors=self.k, metric='chebyshev').fit(Y)
-        
+
         nx = np.array([len(nbrs_x.radius_neighbors([x], radius=epsilon[i], return_distance=False)[0]) - 1 for i, x in enumerate(X)])
         ny = np.array([len(nbrs_y.radius_neighbors([y], radius=epsilon[i], return_distance=False)[0]) - 1 for i, y in enumerate(Y)])
-        
+
         mi = digamma(self.k) - np.mean(digamma(nx + 1)) - np.mean(digamma(ny + 1)) + digamma(n)
         return mi
 
     def calculate(self, X, Y):
+        """
+        Calculate mutual information using the specified method.
+
+        Parameters
+        ----------
+        X : array-like or torch.Tensor
+            Input data.
+        Y : array-like or torch.Tensor
+            Output data.
+        
+        Returns
+        -------
+        mi : float
+            Mutual information between X and Y.
+        """
         if isinstance(X, torch.Tensor):
             X = X.cpu().detach().numpy()
         if isinstance(Y, torch.Tensor):
             Y = Y.cpu().detach().numpy()
-        
-        if X.shape[1] != Y.shape[1]:
-            raise ValueError("Input data and layer data must have the same number of features.")
-        
-        mi_values = []
-        for i in range(X.shape[1]):
-            if self.method == 'binning':
-                mi_values.append(self.mutual_info_bin(X[:, i], Y[:, i], self.bins))
-            elif self.method == 'kde':
-                mi_values.append(self.mutual_info_kde(X[:, i], Y[:, i]))
-            elif self.method == 'kraskov':
-                mi_values.append(self.mutual_info_kraskov(X[:, i], Y[:, i]))
-            else:
-                raise ValueError("Unknown method: choose from 'binning', 'kde', 'kraskov'")
-        
-        return np.mean(mi_values)
+
+        if self.method == 'binning':
+            return self.mutual_info_bin(X, Y, self.bins)
+            # try:
+            #     return self.mutual_info_bin(X, Y, self.bins)
+            # except Exception as e:
+            #     print(f"Error in binning method: {e}")
+            #     return 0.0
+        elif self.method == 'kde':
+            try:
+                return self.kde_mutual_info(X, Y, self.bandwidth)
+            except Exception as e:
+                print(f"Error in KDE method: {e}")
+                return 0.0
+        elif self.method == 'kraskov':
+            try:
+                return self.mutual_info_kraskov(X, Y)
+            except Exception as e:
+                print(f"Error in Kraskov method: {e}")
+                return 0.0
+        else:
+            raise ValueError("Unknown method: choose from 'binning', 'kde', 'kraskov'")
+
+# Example usage with neural network outputs
+if __name__ == '__main__':
+    X = np.random.rand(100,2)
+    Y = X * 0.5 + np.random.rand(100,2) * 0.1
+    
+    mi_calculator = MutualInformationCalculator(method='binning')
+    print(f"Mutual Information (Binning): {mi_calculator.calculate(X, Y)}")
+    
+    mi_calculator = MutualInformationCalculator(method='kde')
+    print(f"Mutual Information (KDE): {mi_calculator.calculate(X, Y)}")
+    
+    mi_calculator = MutualInformationCalculator(method='kraskov')
+    print(f"Mutual Information (Kraskov): {mi_calculator.calculate(X, Y)}")
